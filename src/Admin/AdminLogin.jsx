1eb2bd2b7d31eb1f52a91b246989afeb511936d0
bhargava-projects-07 <iambala.ts@gmail.com>
2026-08-16 17:08:33 +0530

    Admin Login Button Spinning.
    Admin Login Button Spinning.

--- src/Admin/AdminLogin.jsx
+++ src/Admin/AdminLogin.jsx
@@ -96,14 +96,21 @@ const AdminLogin = () => {
                         </div>
 
                         <div className="mb-4 text-center">
-                            <input type='submit' disabled={isSubmitting} value="Login" className={`bg-algae-500 text-algae-900 font-semibold text-black py-2 px-8 rounded-full ${isSubmitting ? 'opacity-70 cursor-not-allowed' : 'cursor-pointer hover:bg-emerald-500'}`} />
 
-                            {isSubmitting && (
-                                <svg className="animate-spin h-4 w-4 text-white" xmlns="http://w3.org" fill="none" viewBox="0 0 24 24">
-                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                </svg>
-                            )}
+                            <button 
+                                type="submit"
+                                disabled={isSubmitting}
+                                className={`bg-algae-500 text-algae-900 font-semibold text-black py-2 px-8 rounded-full ${isSubmitting ? 'opacity-70 cursor-not-allowed' : 'cursor-pointer hover:bg-emerald-500'}`}
+                            >
+                                {isSubmitting && (
+                                    <svg className="animate-spin h-4 w-4 text-white" xmlns="http://w3.org" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                    </svg>
+                                )}
+
+                                <span>{isSubmitting ? 'Checking Login...' : 'Login'}</span>
+                            </button>
 
                         </div>
 
